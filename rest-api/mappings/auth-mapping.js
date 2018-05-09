d03b31cb6b220c94fa7285843750c0a091494c9a
desibankproject <desibankproject@gmail.com>
2018-05-09 13:50:09 -0700

    Reset password

--- rest-api/mappings/auth-mapping.js
+++ rest-api/mappings/auth-mapping.js
@@ -3,5 +3,6 @@ var AuthService = require("../service/auth-service.js");
 module.exports = function(app) {
 	app.post("/email-verification",AuthService.sentEmailVarification);
 	app.post("/auth",AuthService.authUser);
+	app.post("/forget-password",AuthService.forgetPassword);
 	
 };
--- rest-api/service/auth-service.js
+++ rest-api/service/auth-service.js
@@ -2,6 +2,59 @@ var nodemailer = require('nodemailer');
 var UserSchema=require('../../entity/user-schema');
 var ProfileSchema=require('../../entity/profile-schema');
 
+/**
+ * 
+ */
+exports.forgetPassword=function(req,res) {
+	var pusername=req.body.username;
+	ProfileSchema.find({username:pusername},function(error,results){
+		 var data={};
+		if(error){
+			  console.log(error);
+			  data={status:"fail",message:error};
+			  res.status(500).json(data);
+		  }else{
+			  if(results.length===0) {
+				  	data={status:"fail",message:"Sorry , this user does not exist into the database"};
+			  }else{
+				  data={status:"pass",message:"Password is emailed your successfully, please check your email"};
+				  var password=results[0].password;
+				  var email=results[0].email;
+				  //write code to send email
+				//write code to send the email
+				  //Creating instance of the transporter	
+				  var transporter = nodemailer.createTransport({
+						 service: 'gmail',
+						 auth: {
+						        user: 'desibankproject@gmail.com',
+						        pass: 'desibank@123'
+						    }
+					});
+				  //defining the mail options
+				  var mailOptions = {
+						  from: 'nagen@synergisticit.com', // sender address
+						  to: email, // list of receivers
+						  subject: 'Regarding your password in our database...........', // Subject line
+						  html: '<p>Dear  '+pusername+"</p><br/><br/> Your password is  "+password+" <br/><br/> Thanks <br/> Nagendra"// plain text body
+						};
+
+				  transporter.sendMail(mailOptions, function (err, info) {
+								   if(err) {
+								     console.log(err);
+								   } 
+								   else {
+									    console.log("_@_@_@)@@*sending email............");
+									   console.log(info);
+								   } 
+					});
+					
+			  }
+			  console.log("_@_@_@)@@*sending response............");
+			  res.status(200).json(data);
+		  }	  
+	});
+};
+
 exports.authUser=function(req,res) {
 	var pusername=req.body.username;
 	var ppassword=req.body.password;
@@ -44,7 +97,7 @@ exports.sentEmailVarification=function(req,res) {
 			  html: '<p>Test message which you can modify as per your  need!</p> : - - '+message// plain text body
 			};
 	
-	transporter.sendMail(mailOptions, function (err, info) {
+	  transporter.sendMail(mailOptions, function (err, info) {
 			var data={};
 		   if(err) {
 		     console.log(err);
